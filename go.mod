af81fe84f3618f361f20c9999cb682759e7e32e4
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2021-11-05 21:22:26 +0900

    Add Input Capture

--- go.mod
+++ go.mod
@@ -2,11 +2,13 @@ module github.com/kunikuni03/tui-todo
 
 go 1.17
 
-require github.com/rivo/tview v0.0.0-20211029142923-a4acb08f513e
+require (
+	github.com/gdamore/tcell/v2 v2.4.1-0.20210905002822-f057f0a857a1
+	github.com/rivo/tview v0.0.0-20211029142923-a4acb08f513e
+)
 
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/gdamore/tcell/v2 v2.4.1-0.20210905002822-f057f0a857a1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func main() {
 	app := tview.NewApplication()
-	flex := tview.NewFlex().
-		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-			AddItem(tview.NewBox().SetBorder(true).SetTitle("New Task"), 0, 1, false).
-			AddItem(tview.NewBox().SetBorder(true).SetTitle("ToDo List"), 0, 3, false), 0, 2, false)
+
+	listView := tview.NewTextView()
+	listView.SetTitle("ToDo").SetBorder(true)
+
+	textBox := tview.NewInputField()
+	textBox.SetLabel("ToDo: ").SetTitle("New ToDo").SetBorder(true)
+
+	textBox.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyEnter:
+			listView.SetText(listView.GetText(true) + textBox.GetText() + "\n")
+			textBox.SetText("")
+			return nil
+		}
+		return event
+	})
+
+	flex := tview.NewFlex()
+	flex.SetDirection(tview.FlexRow).
+		AddItem(textBox, 4, 0, true).
+		AddItem(listView, 0, 1, false)
+
 	if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
 		panic(err)
 	}
