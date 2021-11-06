714e76b1bd317abf39bfe9fd6328cb927bb21c2e
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2021-11-06 22:33:19 +0900

    add  feature save ToDo

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
+const FILEPATH = "ToDoFile"
+
 func main() {
+	todos, err := readFile()
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	app := tview.NewApplication()
 
 	listView := tview.NewTextView()
 	listView.SetTitle("ToDo").SetBorder(true)
+	listView.SetText(todos)
 
 	textBox := tview.NewInputField()
 	textBox.SetLabel("ToDo: ").SetTitle("New ToDo").SetBorder(true)
@@ -17,7 +29,16 @@ func main() {
 	textBox.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
 		case tcell.KeyEnter:
-			listView.SetText(listView.GetText(true) + textBox.GetText() + "\n")
+			todoText := textBox.GetText()
+			if todoText == "" {
+				return nil
+			}
+
+			if err := writeFile(todoText); err != nil {
+				fmt.Println(err)
+			}
+
+			listView.SetText(listView.GetText(true) + todoText + "\n")
 			textBox.SetText("")
 			return nil
 		}
@@ -32,4 +53,38 @@ func main() {
 	if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
 		panic(err)
 	}
+}
+
+// readFile reads ToDo list from the file.
+func readFile() (string, error) {
+	if _, err := os.Stat(FILEPATH); os.IsNotExist(err) {
+		return "", nil
+	}
+
+	f, err := os.OpenFile(FILEPATH, os.O_RDONLY, 0666)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	buf, err := io.ReadAll(f)
+	if err != nil {
+		return "", err
+	}
+	todos := string(buf)
+	return todos, nil
+}
+
+// writeFile writes ToDo to the file.
+func writeFile(todo string) error {
+	f, err := os.OpenFile(FILEPATH, (os.O_RDWR | os.O_CREATE | os.O_APPEND), 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := fmt.Fprintf(f, todo+"\n"); err != nil {
+		return err
+	}
+	return nil
 }
